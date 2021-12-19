22c0849130917b698cbd91f7279ea7fc9823fd4e
Edgar Mora <ecedgaromar@gmail.com>
2021-12-18 20:10:21 -0700

    Fin sección 7

---
+++ lib/models/usuario.dart
@@ -0,0 +1,12 @@
+class Usuario {
+  String uid;
+  String nombre;
+  String email;
+  bool online;
+
+  Usuario(
+      {required this.uid,
+      required this.nombre,
+      required this.email,
+      required this.online});
+}
